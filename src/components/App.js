98386192c6784032b83b2ab14b8ef17f8b062507
Madhav Milan <49267441+m2-madhav@users.noreply.github.com>
2020-12-05 06:30:37 +0530

    code-added

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,39 @@
 import React, { Component, useState, useEffect } from "react";
-import '../styles/App.css';
+import "../styles/App.css";
 
 const App = () => {
-  // write your code here 
+  // write your code here
+  const [timer, setTimer] = useState("");
+
+  const handleEnter = (event) => {
+    if (event.keyCode === 13) {
+      clearInterval(timerID);
+      if (isNaN(event.target.value)) {
+        setTimer(0);
+        return;
+      }
+      setTimer(parseInt(event.target.value));
+    }
+    return;
+  };
+  const tick = () => {
+    clearInterval(timerID);
+    if (timer <= 0) return;
+    setTimer(timer - 1);
+  };
+  let timerID = setInterval(tick, 1000);
 
   return (
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
+          Reverse countdown for
+          <input id="timeCount" onKeyDown={handleEnter} /> sec.
         </h1>
       </div>
-      <div id="current-time">{/* remaining time */}</div>
+      <div id="current-time">{timer}</div>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
